96df109d582ff752ef91fdc37c2bc896439b27bd
Carlos Sánchez Pérez <carloscsanchezperez@gmail.com>
2012-09-25 19:25:14 +0200

    first commit Rspect Model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Addresses::Application.config.secret_token = 'e8d07d71c59bdb0f65a2b74b4af21b42bda7664284563082d93cb7990925de48f1576bb05ca8eb2d936538e331c9c7a6e47c876e2c0af8aad120f1787ed04ca4'
